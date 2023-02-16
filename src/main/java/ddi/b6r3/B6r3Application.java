32bb9ca051a9f2cb778660d128eaa6ce57f82f0d
Angelcts30 <angel.casans-98@gmail.com>
2023-02-16 20:45:56 +0100

    Subiendo la parte del codigo

---
+++ src/main/java/ddi/b6r3/B6r3Application.java
@@ -0,0 +1,13 @@
+package ddi.b6r3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B6r3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B6r3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ddi/b6r3/B6r3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ddi.b6r3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B6r3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
